55f9bfa303bc6beefa41c9e30f63ebc230f066c4
unknown <michaellang2525@yahoo.com>
2020-01-24 14:41:21 -0500

    creation of file structure and routing

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/app/public"));
+
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
