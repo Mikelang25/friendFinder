13e80e610d8faa9418c8ed83726cc3061e403307
unknown <michaellang2525@yahoo.com>
2020-01-24 16:13:32 -0500

    successfully navigating to the home page and to the survey page when local server is running

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/app/public"));
 
 require('./app/routing/htmlRoutes')(app);
-require('./app/routing/apiRoutes')(app);
 
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
