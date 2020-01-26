c9adf6300f801c6223ef5ff5d9ef5e4929ce374c
unknown <michaellang2525@yahoo.com>
2020-01-26 15:56:08 -0500

    new user's responses are compared to friends.js data and the closest match json object is returned as the response

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+
+
+var friends = [
+    {
+        name: "Adam Sandler",
+        image: "adam@example.com",
+        scores: [
+            2,
+            3,
+            4,
+            3,
+            5,
+            2,
+            1,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Dave Chapelle",
+        image: "adam@example.com",
+        scores: [
+            4,
+            2,
+            1,
+            3,
+            1,
+            2,
+            3,
+            4,
+            1,
+            4
+        ]
+    },
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,22 @@
-var express = require("express");
-var path = require("path");
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.post("/api/survey", function(req, res) {
+        
+        var friendTotal = 100;
+        var closestMatch;
+        var newFriend = req.body.scores
+        for (var i=0;i<friendData.length;i++){
+            var totalScore = 0; 
+            for(var j=0;j<newFriend.length;j++){
+                totalScore += Math.abs(parseInt(newFriend[j])-parseInt(friendData[i].scores[j]))
+            }
+            if(totalScore < friendTotal){
+                friendTotal = totalScore;
+                closestMatch = i;
+            }
+        }
+        res.json(friendData[closestMatch]);        
+    });
+};
